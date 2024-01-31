33e641509caa2e8d99e532b07ee7bc9f29c17fe5
Grimur12 <dgrimur@gmail.com>
2024-01-31 18:44:02 +0000

    Byrja á klasanum

--- Verkefni1/array_list.py
+++ Verkefni1/array_list.py
@@ -12,66 +12,132 @@ class NotOrdered(Exception):
 
 class ArrayList:
     def __init__(self):
-        self.__size = 16
-        # TODO: remove 'pass' and implement functionality
-        self.arr = [0] * self.__size
-        pass
+        self.size = 0 # Byrjar á að vera 0
+        self.arr = [0] * self.size
 
     #Time complexity: O(n) - linear time in size of list
     def __str__(self):
-        # TODO: remove 'pass' and implement functionality
-        return_string = ""
-        return return_string
+        # Bara til að byrja með til að geta testað allt dótið, ég held að þetta megi ekki
+        ret_str = ""
+        for i in range(self.size):
+            ret_str = ret_str + " " + str(self.arr[i])
+        return ret_str
 
     #Time complexity: O(n) - linear time in size of list
     def prepend(self, value):
-        # TODO: remove 'pass' and implement functionality
-        self.value = value
+        # Nota insert fallið til að prependa 
+        # eithvað eins og self.insert(value, 0)
+        self.insert(value,0)
         
 
     #Time complexity: O(n) - linear time in size of list
     def insert(self, value, index):
-        # TODO: remove 'pass' and implement functionality
-        pass
+        # þarf að innihalda if index < 0 eða > self.size þá raise IndexOutOfBounds()
+        # Ef það á að inserta í arrayið þá þýðir það að það er verið að stækka það, erum með function resize sem á að gera það
+        # ef við stækkum alltaf listann um +1 í resize þá gætum við alltaf bara kallað á það í þessu falli
+        # Pæling hvort maður ætti þá að hækka size um +1 í þessu falli eða í resize
+        # í lokinn á insert þarf að bæta 1 við size , self.size += 1
+        
+        """
+        def resize(array):
+            array.append(0)
+            return array
+
+        def insert(array, value, index, size):
+            array = resize(array)
+            for i in range(size, index, -1):
+                array[i] = array[i - 1]
+
+            array[index] = value
+            return array
+
+        arr = [1,2,3,4,5,6,7,8,9,10]
+        arr = insert(arr,23,4,len(arr))
+        print(arr)
+        """
+        # Þetta myndi virka, köllum á resize() fyrst
+        # Ættum að fá nýjann array með auka 0 aftaná megum ekki nota append í þessu samt
+        # Ef við erum alltaf með síðasta stakið 0 aftast þá iterateum við í gegnum arrayið
+        # byrjum á aftasta sem væri þá size, niðri indexinn sem við viljum breyta, og förum alltaf 1 skref til baka með -1
+        # Overrideum síðan stakið sem er á i s.s aftast með stakinu sem er á bavkið þannig hverfur 0'ið aftast og á endanum verður auka stak í indexinum sem við viljum
+        # Þá bara overridea það sér með self.arr[index] = value
+        # Virkar bara ef resize hefur 0 aftaná
+        # síðan self.size += 1
+
+        self.resize()
+
+        for i in range(self.size, index, -1):
+            self.arr[i] = self.arr[i-1]
+        
+        self.arr[index] = value
+
+        self.size += 1
 
     #Time complexity: O(1) - constant time
     def append(self, value):
-        # TODO: remove 'pass' and implement functionality
-        pass
+        # Sama og með prepend
+        # self.insert(value,self.size) # setja stakið aftast á arrayið
+        self.insert(value,self.size)
 
     #Time complexity: O(1) - constant time
     def set_at(self, value, index):
-        # TODO: remove 'pass' and implement functionality
+        # Getum sliceað arrayið og sett í staðin
+        # self.arr[index] = value
+        # if index < 0 eða > self.size þá raise IndexOutOfBounds()
         pass
 
     #Time complexity: O(1) - constant time
     def get_first(self):
-        # TODO: remove 'pass' and implement functionality
+        # Slicea arrayið
+        # return self.arr[0] 
+        # if self.size (stærðin á arrayinu) == 0 þá raise Empty()
         pass
 
     #Time complexity: O(1) - constant time
     def get_at(self, index):
-        # TODO: remove 'pass' and implement functionality
+        # slicea arrayið
+        # getum gert sama og í set_at s.s if index < 0 eða > self.size þá raise IndexOutOfBounds()
+        # return self.arr[index]
         pass
 
     #Time complexity: O(1) - constant time
     def get_last(self):
-        # TODO: remove 'pass' and implement functionality
+        # Slicea arrayið
+        # getum gert sama og í get_first s.s if self.size (stærðin á arrayinu) == 0 þá raise Empty()
+        # return self.arr[-1] eða self.arr[self.size -1] ef [-1] er ekki leyfilegt held að það sé það samt
         pass
 
     #Time complexity: O(n) - linear time in size of list
     def resize(self):
-        # TODO: remove 'pass' and implement functionality
-        pass
+
+        new_arr = [0] * (1 + self.size)
+
+        for i in range(self.size):
+            new_arr[i] = self.arr[i]
+
+        self.arr = new_arr
+
+        # stækka arrayið sé ekki hvort það standi hversu mikið það eigi að vera
+        # gera nýtt array
+        # new_arr = [0] * (1+self.size eða hversu mikið hann á að stækka um)
+        # for loopa yfir gamla arrayið ? kanski má það ekki.. new_array[i] = self.array[i]
+        # Síðan overridea self.array, sef.array = new_array
 
     #Time complexity: O(n) - linear time in size of list
     def remove_at(self, index):
-        # TODO: remove 'pass' and implement functionality
+        # Sama og get_at
+        # Slicea arrayið
+        # Gætum búið til for lykkju sem fer yfir allt arrayið frá indexinum sem við viljum taka stakið frá og uppí endann eða næst síðasta stakið afþví við erum að taka 1 í burtu
+        # síðan overridea stakið sem er á indexinum fyrir næsta stak
+        # Þannig for i in range(index, self.size - 1) og siðan self.arr[i] = self.arr[i+1] síðan þarf að minka size um 1, kanski má þetta ekki, veit ekki hvort það megi hafa for loopur
+        # Önnur leið væri að bara slicea listann uppað indexinu og frá indexinu og bæta saman
+        # self.arr = self.arr[:index] + self.arr[index+1:] og síðan taka 1 fra size, self.size -= 1
         pass
 
     #Time complexity: O(1) - constant time
     def clear(self):
-        # TODO: remove 'pass' and implement functionality
+        # Búa til nýtt self.arr ??
+        # og svo self.size = 0 
         pass
 
     #Time complexity: O(n) - linear time in size of list
@@ -92,10 +158,13 @@ class ArrayList:
 
 
 if __name__ == "__main__":
-    pass
     # add your tests here or in a different file.
     # Do not add them outside this if statement
     # and make sure they are at this indent level
 
     arr_lis = ArrayList()
+    arr_lis.insert(3,0)
+    arr_lis.append(4)
+    arr_lis.prepend(6)
+    arr_lis.insert(9,2)
     print(str(arr_lis))
