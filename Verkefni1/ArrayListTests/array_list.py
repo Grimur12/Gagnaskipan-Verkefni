e2968c9f538e1d1f5dcb050ccd373715f0fdfbde
Rizki Farhan Hauksson <rizkihans1099@gmail.com>
2024-02-01 13:23:23 +0000

    var ap bæta við

--- Verkefni1/ArrayListTests/array_list.py
+++ Verkefni1/ArrayListTests/array_list.py
@@ -13,7 +13,9 @@ class NotOrdered(Exception):
 class ArrayList:
     def __init__(self):
         # TODO: remove 'pass' and implement functionality
-        pass
+        self.size = 0
+        self.capacity = 4
+        self.arr = [None] * self.capacity
 
     #Time complexity: O(n) - linear time in size of list
     def __str__(self):
@@ -24,7 +26,7 @@ class ArrayList:
     #Time complexity: O(n) - linear time in size of list
     def prepend(self, value):
         # TODO: remove 'pass' and implement functionality
-        pass
+        self.insert(value, 0)
 
     #Time complexity: O(n) - linear time in size of list
     def insert(self, value, index):
@@ -34,7 +36,10 @@ class ArrayList:
     #Time complexity: O(1) - constant time
     def append(self, value):
         # TODO: remove 'pass' and implement functionality
-        pass
+        if self.size + 1 > self.capacity:
+            self.rezise()
+        self.arr[self.size] = value
+        self.size += 1
 
     #Time complexity: O(1) - constant time
     def set_at(self, value, index):
@@ -59,7 +64,13 @@ class ArrayList:
     #Time complexity: O(n) - linear time in size of list
     def resize(self):
         # TODO: remove 'pass' and implement functionality
-        pass
+        # Re-allocates memory for a larger array and populates it with the original array’s items
+        self.capacity = self.capacity * 2
+        new_arr = [None] * self.capacity
+        for i in range(self.size):
+            new_arr[i] = self.arr[i]
+        self.arr = new_arr
+        
 
     #Time complexity: O(n) - linear time in size of list
     def remove_at(self, index):
