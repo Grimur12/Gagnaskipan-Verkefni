99e0461a901e600e02fd309c7db8c33e51211ffe
Grimur12 <dgrimur@gmail.com>
2024-02-27 21:50:02 +0000

    tilbuið

--- DLL_base/dll.py
+++ DLL_base/dll.py
@@ -12,8 +12,6 @@ class DLL:
         self.head.next = self.tail
         self.tail.prev = self.head
         self.current_position = 0
-        # [ -1 , 0, 1, 2, 3, self.size = 4 ]
-        # [HEAD, 1, 2, 3, 4, TAIL]
         self.current_node = self.tail
         self.size = 0
 
@@ -34,27 +32,12 @@ class DLL:
             self.current_node = self.current_node.next
             self.current_position -= 1
             self.size -= 1
-        # Þetta þarf að laga
-        # if self.size == 0:
-        #     return
-        # if self.current_node.next:
-        #     self.current_node.prev.next = self.current_node.next
-        #     self.current_node.next.prev = self.current_node.prev
-        #     self.current_node = self.current_node.next
-        #     self.current_position -= 1
-        # else:
-        #      self.current_node = self.current_node.prev
-        #      self.current_node.prev.next = self.current_node.next
-        #      self.current_node.next.prev = self.current_node.prev
-        #      self.current_node = self.current_node.next
-        #      self.current_position += 1
-        # self.size -= 1
-
+            
     def get_value(self):
         return self.current_node.data       
 
     def move_to_next(self):
-        if self.current_node == self.tail: # Testcase virkar ef ég leyfi move_to_next að fara á self.tail
+        if self.current_node == self.tail: 
             return
         else:
             self.current_node = self.current_node.next
@@ -68,7 +51,6 @@ class DLL:
             self.current_position +=1
 
     def move_to_pos(self, pos):
-        # Það þarf að breyta þessu aðeins, gera flottara
         if pos > self.size or pos <= -1:
             return
         current_pos = 0
@@ -81,6 +63,8 @@ class DLL:
         
 
     def clear(self):
+        # Væri líka hægt að bara self.__init__()
+        # Erum i raun að gera það sama hér
         self.head.next = self.tail
         self.tail.prev = self.head
         self.current_position = 0
@@ -125,16 +109,16 @@ class DLL:
         next.data = temp_data
 
     def sort(self):
-        # Virkar en er ekki quick sort
         first = self.get_first_node()
         last = self.get_last_node()
         
         curr_node = first
-
+        # Við höldum áfram að iteratea þangað til curr_node er orðið að tail
         while curr_node != last.next and curr_node.data:
 
             next_node = curr_node.next
-
+    	    # Iteratear þangað til next_node er orðið að tail, þar ætti amk fyrsta stakið að vera á réttum stað, þá þarf að gá að næsta staki í listanum og sorta
+            # næsta nóða curr_node = curr_node.next sem verður þá næsta stak til að skoða og byrja innri loopuna aftur, heldur áfram þangað til við hittum á ytri loopuna
             while next_node != last.next and next_node.data:
                 if curr_node.data > next_node.data:
                     self.swap_data(curr_node, next_node)
@@ -144,10 +128,6 @@ class DLL:
             curr_node = curr_node.next
 
         self.current_node = self.head.next
-
-
-    def quicksort(low, high):
-        pass
 
     def __len__(self):
         return self.size
@@ -161,50 +141,4 @@ class DLL:
         return ret_str
 
 if __name__ == "__main__":
-    dll = DLL()
-    dll.insert(1)
-    dll.insert(3)
-    dll.insert(4)
-    dll.insert(2)
-    dll.insert(7)
-    dll.insert(6)
-    dll.insert(5)
-    dll.insert(10)
-    dll.insert(8)
-    dll.insert(9)
-    dll.insert(12)
-    dll.insert(11)
-    dll.insert(15)
-    dll.insert(13)
-    dll.insert(14)
-    dll.insert(16)
-    print(dll)
-    print(dll.get_first_node().data)
-    print(dll.get_last_node().data)
-    dll.partition(dll.get_first_node(),dll.get_last_node())
-    print(dll)
-    dll.sort()
-    print(dll)
-    # dll = DLL()
-    # print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
-    # dll.insert("A")
-    # print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
-    # dll.insert("B1")
-    # print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
-    # dll.insert("C")
-    # print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
-    # dll.insert("A")
-    # print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
-    # dll.insert("B2")
-    # print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
-    # dll.partition(dll.get_first_node(), dll.get_last_node())
-    # print(str(dll) + "   -   current value: " + str(dll.get_value()) + "   -   size: " + str(len(dll)))
-
-
-def quick_sort(sequence):
-    length = len(sequence)
-    if length <= 1:
-        return sequence
-    else:
-        pivot = sequence.pop()
-        
+    pass
