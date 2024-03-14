c010c585581a4a9e64a9665fb35c74b496229adb
Rizki Farhan Hauksson <rizkihans1099@gmail.com>
2024-03-14 18:33:03 +0000

    aðeins að laga

--- PA5/myhashablekey.py
+++ PA5/myhashablekey.py
@@ -7,5 +7,10 @@ class MyHashableKey:
         return self.i == other.i and self.s == other.s
     
     def __hash__(self):
-        return self.i % 10
+        return self.i + sum([ord(c) for c in self.s]) 
+    # Þetta er hashið sem við notum til að hasha hlutina okkar. 
+    # Við notum sum til að summa upp öll ascii gildi stafanna í strengnum og 
+    # svo plúsum við við heiltöluna sem við fengum. Þetta er svo hashið sem 
+    # við notum til að hasha hlutina okkar.
+    
     
